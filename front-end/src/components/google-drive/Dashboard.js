1da663c293e8fe1c11f45c03bfe2cba693557416
DouglasMacKrell <douglasmackrell@pursuit.org>
2021-02-13 18:15:59 -0500

    breadcrumbs init

--- front-end/src/components/google-drive/Dashboard.js
+++ front-end/src/components/google-drive/Dashboard.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { useFolder } from '../../hooks/useFolder'
 import Navbar from './Navbar'
 import AddFolderButton from './AddFolderButton'
 import Folder from './Folder'
+import FolderBreadcrumbs from './FolderBreadcrumbs'
 
 
 export default function Dashboard() {
-    const { folder, childFolders } = useFolder()
+    const { folderId } = useParams()
+    const { folder, childFolders } = useFolder(folderId)
     console.log(childFolders)
 
     return (
         <>
             <Navbar />
             <Container fluid>
-                <AddFolderButton currentFolder={folder} />
+                <div className="d-flex align-items-center">
+                    <FolderBreadcrumbs currentFolder={folder} />
+                    <AddFolderButton currentFolder={folder} />
+
+                </div>
                 {childFolders.length > 0 && (
                     <div className="d-flex flex-wrap">
                         {childFolders.map(childFolder => (
---
+++ front-end/src/components/google-drive/FolderBreadcrumbs.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Breadcrumb } from 'react-bootstrap'
+import { ROOT_FOLDER } from '../../hooks/useFolder'
+import { Link } from 'react-router-dom'
+
+export default function FolderBreadcrumbs({ currentFolder }) {
+    let path = currentFolder === ROOT_FOLDER ? [] : [ROOT_FOLDER]
+    if (currentFolder) path = [...path, ...currentFolder.path]
+
+    return (
+        <Breadcrumb className="flex-grow-1" listProps={{ className: "bg-white pl-0 m-0" }}>
+            {path.map((folder, index) => (
+                <Breadcrumb.Item
+                    key={folder.id}
+                    linkProps={{
+                        to: `folders/${folder.id}`
+                    }}
+                    linkAs={Link}
+                    className="text-truncate d-inline-block"
+                    style={{ maxWidth: "150px" }}
+                    active
+                    >
+                        {folder.name}
+                </Breadcrumb.Item>
+            ))}
+            {currentFolder && (
+                <Breadcrumb.Item className="text-truncate d-inline-block" style={{ maxWidth: "200px" }} active>
+                    {currentFolder.name}
+                </Breadcrumb.Item>
+            )}
+        </Breadcrumb>
+    )
+}
